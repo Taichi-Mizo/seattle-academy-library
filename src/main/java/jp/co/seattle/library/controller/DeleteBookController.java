6265c80570e3b6eec1a1fa84f70908b9b6dff0a3
TAICHI MIZO <user@usernoMacBook-Air.local>
2021-04-21 17:35:20 +0900

    タスク４完了しました

--- src/main/java/jp/co/seattle/library/controller/DeleteBookController.java
+++ src/main/java/jp/co/seattle/library/controller/DeleteBookController.java
@@ -1,15 +1,20 @@
 package jp.co.seattle.library.controller;
 
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jp.co.seattle.library.dto.BookInfo;
+import jp.co.seattle.library.service.BooksService;
 
 /**
  * 削除コントローラー
@@ -18,6 +23,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class DeleteBookController {
     final static Logger logger = LoggerFactory.getLogger(DeleteBookController.class);
 
+    @Autowired
+    private BooksService booksService;
 
     /**
      * 対象書籍を削除する
@@ -29,15 +36,23 @@ public class DeleteBookController {
      */
     @Transactional
     @RequestMapping(value = "/deleteBook", method = RequestMethod.POST)
+
     public String deleteBook(
             Locale locale,
             @RequestParam("bookId") Integer bookId,
             Model model) {
         logger.info("Welcome delete! The client locale is {}.", locale);
-
-
-        return "";
-
+        //画面からのリクエストの値、SpringFrameWork
+        
+        booksService.deleteBookInfo(bookId);
+                
+        //削除後の状態を画面に反映させるべく、削除後のSQLから書籍情報を取得する。
+        //戻り値を受け取る
+        List<BookInfo> newBookList = booksService.getBookList();
+        
+        model.addAttribute("bookList", newBookList);
+        
+        return "home";
     }
 
 }
